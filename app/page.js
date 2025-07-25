a73536e157a6efbea03409dbcdb2af3a42d14f16
nov <novslog@gmail.com>
2025-07-25 19:46:48 +0900

    main ui 변경

--- app/page.js
+++ app/page.js
@@ -21,28 +21,39 @@ export default function HomePage() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.titleContainer}>
-        <img src="/img/lacuns.jpg" alt="Vocun Logo" className={styles.logo} />
-        <h1 className={styles.title}>Vocun</h1>
-      </div>
-      
-      <div className={styles.formContainer}>
-        <input
-            type="text"
-            value={newWordbookName}
-            onChange={(e) => setNewWordbookName(e.target.value)}
-            placeholder="새 단어장 이름"
-            className={styles.input}
-            onKeyPress={(e) => e.key === 'Enter' && handleAddWordbook()}
-        />
-        <button onClick={handleAddWordbook} className={styles.button}>새 단어장 추가</button>
+      <div className={styles.mainContentWrapper}>
+        <div className={styles.titleContainer}>
+          <img src="/img/lacuns.jpg" alt="Vocun Logo" className={styles.logo} />
+          <h1 className={styles.title}>Vocun</h1>
+        </div>
+        
+        <div className={styles.formContainer}>
+          <input
+              type="text"
+              value={newWordbookName}
+              onChange={(e) => setNewWordbookName(e.target.value)}
+              placeholder="새 단어장 이름"
+              className={styles.input}
+              onKeyPress={(e) => e.key === 'Enter' && handleAddWordbook()}
+          />
+          <button onClick={handleAddWordbook} className={styles.addButton}>
+            <img src="/img/leaf.png" alt="새 단어장 추가" className={styles.addIcon} />
+          </button>
+        </div>
+
+        <div className={styles.manageButtonContainer}>
+          <Link href="/wordbooks" className={`${styles.button} ${styles.manageButton}`}>
+            단어장 관리
+          </Link>
+        </div>
       </div>
 
-      <div className={styles.manageButtonContainer}>
-        <Link href="/wordbooks" className={`${styles.button} ${styles.manageButton}`}>
-          단어장 관리
-        </Link>
-      </div>
+      <footer className={styles.footer}>
+        <p>Developer Contact:</p>
+        <p><img src="/img/envelope.png" alt="Email" className={styles.footerIcon} /> Email: <a href="mailto:dohana1205@gmail.com">dohana1205@gmail.com</a></p>
+        <p><img src="/img/instagram.png" alt="Instagram" className={styles.footerIcon} /> Instagram: <a href="https://www.instagram.com/doil_0213" target="_blank" rel="noopener noreferrer">doil_0213</a></p>
+        <p><img src="/img/blogger.png" alt="Blog" className={styles.footerIcon} /> Blog: <a href="https://novlog.tistory.com/" target="_blank" rel="noopener noreferrer">novlog.tistory.com</a></p>
+      </footer>
     </div>
   );
 }
